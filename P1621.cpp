ce1d97a9fad82aaa14180f8cbc1e7340bbbff9e5
1792838722 <1792838722@qq.com>
2023-03-30 10:51:18 +0800

    3位整数的各位和

--- P1621.cpp
+++ P1621.cpp
@@ -1,1 +1,32 @@
 #include<iostream>
+int x;
+class Number{
+    public:
+        Number(int x)
+        {
+            num = x;
+        }
+        void getAns()
+        {
+            std::cout << getBit() + getBit() + getBit() << std::endl;
+        }
+    private:
+        int num;
+        int getBit()
+        {
+            return num%10 + divide();
+        }
+        int divide()
+        {
+            num /= 10;
+            return 0;
+        }
+};
+int main()
+{
+    std::cin >> x;
+    Number *nums = new Number(x);
+    nums -> getAns();
+    delete nums;
+    return 0;
+}
