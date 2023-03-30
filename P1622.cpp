f4f834f5d13b930f5647a1e46b7d7da1ca91e33c
1792838722 <1792838722@qq.com>
2023-03-30 11:39:01 +0800

    只出现2次的数

---
+++ P1622.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstring>
+class Number{
+    public:
+        Number(int size)
+        {
+            this->size = size;
+            memset(bucket,0,sizeof bucket);
+        }
+        void init()
+        {
+            for(int i = 1;i <= size;i++)
+            {
+                std::cin >> numbers[i];
+                bucket[numbers[i]]++;
+            }
+        }
+        void print()
+        {
+            for(int i = 1;i <= size;i++)
+            {
+                if(bucket[numbers[i]] == 2)
+                {
+                    std::cout << numbers[i] << " ";
+                    bucket[numbers[i]] = 0;
+                    numbers[0] = -1;
+                }
+            }
+            if(numbers[0] == -1)
+                std::cout << "none";
+        }
+    private:
+        int size,bucket[1005];
+        int numbers[25];
+};
+int size,numbersNeedInput;
+int main()
+{
+    std::cin >> size;
+    Number *nums = new Number(size);
+    nums->init();
+    nums->print();
+    delete nums;
+    system("pause");
+    return 0;
+}
